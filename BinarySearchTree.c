26d7492f30f51b1ce809797132c86ef583654a11
Minh <mindt24082002@gmail.com>
2021-07-20 10:04:02 +0700

    Implement Queue and Level Order Traversal for BST

--- BinarySearchTree.c
+++ BinarySearchTree.c
@@ -1,104 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
-
-struct BstNode
-{
-    int data;
-    struct BstNode *left;
-    struct BstNode *right;
-};
-
-struct BstNode *GetNewNode(int data)
-{
-    struct BstNode *newNode = (struct BstNode *)malloc(sizeof(struct BstNode));
-    newNode->data = data;
-    newNode->left = NULL;
-    newNode->right = NULL;
-    return newNode;
-}
-
-int FindMin(struct BstNode *root)
-{
-
-    // Recursive Approach
-    /*
-    if (root == NULL)
-    {
-        printf("Tree is empty\n");
-        return -1;
-    }
-    else if (root->left == NULL)
-    {
-        return root->data;
-    }
-    else
-    {
-        return FindMin(root->left);
-    }
-    */
-
-    // Iterative Approach
-    if (root == NULL)
-    {
-        printf("Tree is empty\n");
-        return -1;
-    }
-    // struct BstNode* current = root;
-    // We can use root directly because root is just a local variable
-    while (root->left != NULL)
-    {
-        root = root->left;
-    }
-    return root->data;
-};
-
-struct BstNode *Insert(struct BstNode *root, int data)
-{
-    if (root == NULL)
-    {
-        root = GetNewNode(data);
-    }
-    else if (data <= root->data)
-    {
-        root->left = Insert(root->left, data);
-    }
-    else
-    {
-        root->right = Insert(root->right, data);
-    }
-    return root;
-}
-
-bool Search(struct BstNode *root, int data)
-{
-    if (root == NULL)
-        return false;
-    if (root->data == data)
-        return true;
-    else if (data <= root->data)
-    {
-        return Search(root->left, data);
-    }
-    else
-    {
-        return Search(root->right, data);
-    }
-}
-
-void PrintTree(struct BstNode *root)
-{
-    if (root == NULL)
-    {
-        return;
-    }
-    else
-    {
-        printf("%d ", root->data);
-        PrintTree(root->left);
-        PrintTree(root->right);
-    }
-}
+#include "BinarySearchTree.h"
 
 int main()
 {
@@ -109,9 +12,17 @@ int main()
     root = Insert(root, 25);
     root = Insert(root, 8);
     root = Insert(root, 12);
-    PrintTree(root);
+
+    printf("Preorder: ");
+    PreOrder(root);
     printf("\n");
 
+    printf("Levelorder: ");
+    LevelOrder(root);
+    printf("\n");
+
+    // Search a number
+    /*
     int number;
     printf("Enter a number to search: ");
     scanf("%d", &number);
@@ -119,8 +30,13 @@ int main()
         printf("Found\n");
     else
         printf("Not found\n");
+    */
 
+    // Find Min value and Height of the tree
+    /*
     printf("Minimum value of the tree: %d\n", FindMin(root));
+    printf("Height of the tree: %d\n", FindHeight(root));
+    */
 
     return 0;
 }
---
+++ BinarySearchTree.h
@@ -0,0 +1,221 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+struct BstNode
+{
+    int data;
+    struct BstNode *left;
+    struct BstNode *right;
+};
+
+struct Node
+{
+    struct BstNode *BstNodePtr;
+    struct Node *next;
+};
+
+// struct BstNode* Front(front) {
+
+// }
+
+bool IsEmpty(struct Node *front, struct Node *rear)
+{
+    if (front == NULL && rear == NULL)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void Enqueue(struct BstNode *x, struct Node **frontPtr, struct Node **rearPtr)
+{
+    struct Node *temp = (struct Node *)malloc(sizeof(struct Node));
+    temp->BstNodePtr = x;
+    temp->next = NULL;
+    if (IsEmpty(*frontPtr, *rearPtr))
+    {
+        *frontPtr = *rearPtr = temp;
+        return;
+    }
+    else
+    {
+        (*rearPtr)->next = temp;
+        *rearPtr = temp;
+    }
+}
+
+void Dequeue(struct Node **frontPtr, struct Node **rearPtr)
+{
+    struct Node *temp = *frontPtr;
+    if (IsEmpty(*frontPtr, *rearPtr))
+    {
+        printf("Queue is empty, nothing to dequeue. \n");
+        return;
+    }
+    else if (*frontPtr == *rearPtr)
+    {
+        *frontPtr = *rearPtr = NULL;
+    }
+    else
+    {
+        *frontPtr = (*frontPtr)->next;
+    }
+    free(temp);
+}
+
+void PrintQueue(struct Node *front, struct Node *rear)
+{
+    if (!IsEmpty(front, rear))
+    {
+        struct Node *temp = front;
+        printf("Queue is: ");
+        while (temp != NULL)
+        {
+            printf("%d ", temp->BstNodePtr->data);
+            temp = temp->next;
+        }
+        printf("\n");
+    }
+}
+
+struct BstNode *GetNewNode(int data)
+{
+    struct BstNode *newNode = (struct BstNode *)malloc(sizeof(struct BstNode));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+int FindMin(struct BstNode *root)
+{
+
+    // Recursive Approach
+    /*
+    if (root == NULL)
+    {
+        printf("Tree is empty\n");
+        return -1;
+    }
+    else if (root->left == NULL)
+    {
+        return root->data;
+    }
+    else
+    {
+        return FindMin(root->left);
+    }
+    */
+
+    // Iterative Approach
+    if (root == NULL)
+    {
+        printf("Tree is empty\n");
+        return -1;
+    }
+    // struct BstNode* current = root;
+    // We can use root directly because root is just a local variable
+    while (root->left != NULL)
+    {
+        root = root->left;
+    }
+    return root->data;
+};
+
+int FindHeight(struct BstNode *root)
+{
+    if (root == NULL)
+    {
+        return -1;
+    }
+    int leftHeight = FindHeight(root->left);
+    int rightHeight = FindHeight(root->right);
+
+    if (leftHeight > rightHeight)
+    {
+        return leftHeight + 1;
+    }
+    return rightHeight + 1;
+}
+
+struct BstNode *Insert(struct BstNode *root, int data)
+{
+    if (root == NULL)
+    {
+        root = GetNewNode(data);
+    }
+    else if (data <= root->data)
+    {
+        root->left = Insert(root->left, data);
+    }
+    else
+    {
+        root->right = Insert(root->right, data);
+    }
+    return root;
+}
+
+bool Search(struct BstNode *root, int data)
+{
+    if (root == NULL)
+        return false;
+    if (root->data == data)
+        return true;
+    else if (data <= root->data)
+    {
+        return Search(root->left, data);
+    }
+    else
+    {
+        return Search(root->right, data);
+    }
+}
+
+void PreOrder(struct BstNode *root)
+{
+    /*
+    Tree Traversal:
+    Bread First => Level Order
+    Depth First:
+            <root> <left> <right> => Preorder
+            <left> <root> <right> => Inorder
+            <left> <right> <root> => Postorder
+    */
+    if (root == NULL)
+    {
+        return;
+    }
+    else
+    {
+        printf("%d ", root->data);
+        PreOrder(root->left);
+        PreOrder(root->right);
+    }
+}
+
+void LevelOrder(struct BstNode *root)
+{
+
+    struct Node *front = NULL;
+    struct Node *rear = NULL;
+
+    Enqueue(root, &front, &rear);
+    while (!IsEmpty(front, rear))
+    {
+        struct BstNode *current = front->BstNodePtr;
+        printf("%d ", current->data);
+        if (current->left != NULL)
+        {
+            Enqueue(current->left, &front, &rear);
+        }
+        if (current->right != NULL)
+        {
+            Enqueue(current->right, &front, &rear);
+        }
+        Dequeue(&front, &rear);
+    }
+}
